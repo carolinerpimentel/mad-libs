285d8743d7dcf3943be3ae278947005713825009
Caroline <carol-html@hotmail.com>
2025-01-14 20:16:47 -0300

    Create MadLibs

---
+++ principalprogram/MadLibs.java
@@ -0,0 +1,53 @@
+package principalprogram;
+
+public class MadLibs {
+    /*
+    This program generates a mad libbed story.
+    Author: Caroline
+    Date: 01/13/2025
+    */
+        public static void main(String[] args){
+         
+        /* 
+        me 
+        String name1 = "Aurora"; 
+        String name2 = "Pedro"; 
+        String adjective1 = "Cheirosa"; 
+        String adjective2 = "Bom";
+        String adjective3 = "Irritante";
+        String noun1 = "mesa"; 
+        String noun2 = "caderno"; 
+        String noun3 = "folha"; 
+        String noun4 = "gelo"; 
+        String noun5 = "comida"; 
+        String noun6 = "pente"; 
+        String verb1 = "agachar"; 
+        String place1 = "Japão";
+        int number = 12;
+        */
+  
+        //other person
+  
+        String name1 = "Ana"; 
+        String name2 = "Carlos"; 
+        String adjective1 = "brilhante"; 
+        String adjective2 = "veloz";
+        String adjective3 = "curioso";
+        String noun1 = "livro"; 
+        String noun2 = "mesa"; 
+        String noun3 = "árvore"; 
+        String noun4 = "relógio"; 
+        String noun5 = "escola"; 
+        String noun6 = "janela"; 
+        String verb1 = "correr"; 
+        String place1 = "parque";
+        int number = 7;
+  
+        //The template for the story
+  
+        String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+      
+        System.out.println(story);
+      }       
+  }
+  
